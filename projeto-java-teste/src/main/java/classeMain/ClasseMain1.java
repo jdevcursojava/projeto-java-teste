80f18ac1fef0487ba96a686852d1755414d52897
alex_ <alex.fernando.egidio@gmail.com>
2026-06-12 22:16:36 -0300

    dd

--- projeto-java-teste/src/main/java/classeMain/ClasseMain1.java
+++ projeto-java-teste/src/main/java/classeMain/ClasseMain1.java
@@ -3,7 +3,7 @@ package classeMain;
 public class ClasseMain1 {
 
 	public static void main(String[] args) {
-		System.out.println("Olá Java ddfd    sdsdsd dd dd dd ");
+		System.out.println("Olá Java ddfd  d   sdsdsd dd dd dd ");
 		System.out.println("Olá Java GitHub - Cobrança ds");
 		System.out.println("Olá Java GitHub - Cobrança Correções");
 	}
